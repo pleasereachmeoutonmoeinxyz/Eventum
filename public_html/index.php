a8a5cc78baaef16ccaa1c249164f80091747e51f
moein7tl <moein7tl@gmail.com>
2014-04-08 16:14:18 +0430

    dummy

--- public_html/index.php
+++ public_html/index.php
@@ -1,57 +1,24 @@
 <?php
 
-define('SALT','HIHI');
-require_once './../vendor/autoload.php';
-$app    =   new Silex\Application();
-$app['debug'] = true;
+define( 'PATH_ROOT',            dirname( __DIR__ ) );
+define( 'PATH_PROTECTED',       PATH_ROOT       . '/protected' );
+define( 'PATH_RUNTIME',         PATH_ROOT       . '/runtime' );
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Validator\Constraints as Assert;
-use RedBean_Facade as R;
+define( 'PATH_CACHE',           PATH_RUNTIME    . '/cache' );
+define( 'PATH_LOG',             PATH_RUNTIME    . '/log' );
 
-$app->register(new Ivoba\Silex\RedBeanServiceProvider(), array(
-    'db.options' => array( 
-        'dsn'       =>  'mysql:host=localhost;dbname=eventmail' ,
-        'username'  =>  'root',
-        'password'  =>  'hitler2013'
-    )));
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-$app->register(new Silex\Provider\ValidatorServiceProvider());
-$app['db']; // init RedBean
+define( 'PATH_PUBLIC',          PATH_ROOT       . '/public_html' );
+define( 'PATH_VENDOR',          PATH_ROOT       . '/vendor' );
+
+define( 'PATH_LOCALES',         PATH_PROTECTED    . '/locales' );
+define( 'PATH_VIEWS',           PATH_PROTECTED    . '/views' );
 
 
-$app->get('/', function() use ($app){
-    return $app['twig']->render('index.html');
-});
+require_once PATH_VENDOR . '/autoload.php';
 
-$app->post('/subscribe',function(Request $request) use ($app){
-    $email      =   $request->get('email');
-    $errors     =   $app['validator']->validateValue($email, new Assert\Email());
-    if (count($errors)){
-        return json_encode(array('success'=>FALSE,'error'=>'wrong-mail'));
-    } else {
-        if (R::count('email', ' email=:email', array(':email'=>$email))){
-            return json_encode(array('success'=>'FALSE','error'=>'exist'));
-        } else {
-            $bean           =   R::dispense('email');
-            $bean->email    =   $email;
-            $bean->param1   =   md5($email.SALT);
-            $bean->param2   =   md5(microtime());
-            $bean->rnd      =   rand(0, PHP_INT_MAX);
-            $id             =   R::store($bean);
-            
-            return json_encode(array(
-                'success'   =>  TRUE,
-                'params'    =>  array(
-                                    'id'        =>  $id,
-                                    'param1'    =>  $bean->param1,
-                                    'param2'    =>  $bean->param2,
-                                    'rnd'       =>  $rnd
-                                )
-                ));
-        }
-    }
-});
-$app->run();
+$app = new Silex\Application();
+
+require PATH_PROTECTED . '/config/development.php';
+require PATH_PROTECTED . '/app.php';
+
+$app['http_cache']->run();

6cd6d1a0337a735c198d1da859f6d4715da8ae10
moein7tl <moein7tl@gmail.com>
2014-04-08 12:06:15 +0430

    init config files

---
+++ protected/config/testing.php
@@ -0,0 +1,14 @@
+<?php
+$app['locale']                  = 'fa';
+$app['translator.messages'] = array(
+	'fa' => PATH_LOCALES . '/fa.yml',
+        'en' => PATH_LOCALES . '/en.yml',
+);
+$app['session.default_locale']  = $app['locale'];
+
+// Cache
+$app['cache.path']              = PATH_CACHE;
+$app['http_cache.cache_dir']    = $app['cache.path'] . '/http';
+$app['twig.options.cache']      = $app['cache.path'] . '/twig';
+
+$app['debug'] = true;

d442c67bd0fb5605037d34b55be753e337d8dc5b
moein7tl <moein7tl@gmail.com>
2014-04-08 16:14:05 +0430

    dummy

--- protected/app.php
+++ protected/app.php
@@ -1,8 +1,46 @@
 <?php
+//
+use Silex\Provider\FormServiceProvider;
+use Silex\Provider\HttpCacheServiceProvider;
+use Silex\Provider\MonologServiceProvider;
+use Silex\Provider\SecurityServiceProvider;
+use Silex\Provider\SessionServiceProvider;
+use Silex\Provider\TranslationServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\UrlGeneratorServiceProvider;
+use Silex\Provider\ValidatorServiceProvider;
+use Symfony\Component\Translation\Loader\YamlFileLoader;
+//
+$app->register(new HttpCacheServiceProvider());
+$app->register(new SessionServiceProvider());
+$app->register(new ValidatorServiceProvider());
+$app->register(new FormServiceProvider());
+$app->register(new UrlGeneratorServiceProvider());
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// Translation definition.
+$app->register(new TranslationServiceProvider());
+$app['translator'] = $app->share($app->extend('translator', function($translator, $app) {
+	$translator->addLoader( 'yaml', new YamlFileLoader() );
+	$translator->addResource( 'yaml', PATH_LOCALES . '/en.yml', 'en' );
+	return $translator;
+}));
 
+// Log definition.
+$app->register(new MonologServiceProvider(), array(
+	'monolog.logfile'   => PATH_LOG . '/app.log',
+	'monolog.name'      => 'app',
+	'monolog.level'     => Monolog\Logger::WARNING 
+));
+
+// Template system definition.
+$app->register(new TwigServiceProvider(), array(
+	'twig.options'		=> array(
+		'cache'			=> isset($app['twig.options.cache']) ? $app['twig.options.cache'] : false,
+		'strict_variables'      => true
+	),
+	'twig.path'		=> array( PATH_VIEWS )
+));
+
+require PATH_PROTECTED . '/routes.php';
+
+return $app;

8a4354fb8511fdb362015481ef565f9620ad2e21
moein7tl <moein7tl@gmail.com>
2014-04-08 16:13:46 +0430

    renew mail controller

--- protected/controller/MailController.php
+++ protected/controller/MailController.php
@@ -5,14 +5,9 @@ namespace Controller{
     use Silex\ControllerProviderInterface;
     class MailController implements ControllerProviderInterface{
         public function connect(Application $app) {
-            $mailController = $app['controllers_factory'];
-            $mailController->post('/subscribe', array($this,'login'))->bind('subscribe');
-            $mailController->get('/unsubscribe',array($this,'unsubscribe'))->bind('unsubscribe');
-            $mailController->get('/setting',array($this,'setting'))->bind('setting');
-        }
-        
-        function setting(Application $app){
+            $controller =   $app['controllers_factory'];
             
+            return $controller;
         }
     }
 }

f1d1533f4e5a3ffbbb8dd01ca7aa0a8906c9e921
moein7tl <moein7tl@gmail.com>
2014-04-10 22:56:01 +0430

    add MailController to router

--- protected/routes.php
+++ protected/routes.php
@@ -1,3 +1,6 @@
 <?php
 require PATH_PROTECTED . '/controller/IndexController.php';
+require PATH_PROTECTED . '/controller/MailController.php';
+
 $app->mount("/",new Controller\IndexController());
+$app->mount("/mail", new Controller\MailController());

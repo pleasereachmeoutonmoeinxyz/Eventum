94f08ed9065cf9b0696852973b3a3218f39334ac
moein7tl <moein7tl@gmail.com>
2014-04-12 14:17:06 +0430

    add email validator

--- protected/controller/IndexController.php
+++ protected/controller/IndexController.php
@@ -3,7 +3,7 @@
 namespace Controller{
     use Silex\Application;
     use Silex\ControllerProviderInterface;
-    
+    use Symfony\Component\Validator\Constraint as Assert;
     class IndexController implements ControllerProviderInterface{
         public function connect(Application $app) {
             $controller =   $app['controllers_factory'];
@@ -13,7 +13,10 @@ namespace Controller{
         
         function index(Application $app){
             $form   =   $app['form.factory']->createBuilder('form')
-                        ->add('email','email',array('label'=>false),array('dir'=>'ltr'))
+                        ->add('email','email',
+                                array(
+                                    'constraints'   =>  array(new \Symfony\Component\Validator\Constraints\Email()),
+                                    'label'=>false))
                         ->getForm();
             
             return $app['twig']->render('index.html',array(

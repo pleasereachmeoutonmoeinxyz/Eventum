c82ac7e857c336aea6636bd6d6fc2f8c291326c4
moein7tl <moein7tl@gmail.com>
2014-04-12 15:07:58 +0430

    remove validator from controller

--- protected/controller/IndexController.php
+++ protected/controller/IndexController.php
@@ -3,7 +3,6 @@
 namespace Controller{
     use Silex\Application;
     use Silex\ControllerProviderInterface;
-    use Symfony\Component\Validator\Constraint as Assert;
     class IndexController implements ControllerProviderInterface{
         public function connect(Application $app) {
             $controller =   $app['controllers_factory'];
@@ -13,10 +12,7 @@ namespace Controller{
         
         function index(Application $app){
             $form   =   $app['form.factory']->createBuilder('form')
-                        ->add('email','email',
-                                array(
-                                    'constraints'   =>  array(new \Symfony\Component\Validator\Constraints\Email()),
-                                    'label'=>false))
+                        ->add('email','email', array('label'=>false))
                         ->getForm();
             
             return $app['twig']->render('index.html',array(

51c2458f694c5946569d85a972fba9c6b200df48
moein7tl <moein7tl@gmail.com>
2014-04-12 14:24:27 +0430

    dummy

--- protected/controller/MailController.php
+++ protected/controller/MailController.php
@@ -3,17 +3,16 @@
 namespace Controller{
     use Silex\Application;
     use Silex\ControllerProviderInterface;
+    use Symfony\Component\HttpFoundation\Request;
     class MailController implements ControllerProviderInterface{
         public function connect(Application $app) {
             $controller =   $app['controllers_factory'];
             
-            $controller->post('/subscribe',array($this,'subscribe'))->bind('subscribe');
+            $controller->post('/subscribe',  function (Request $request) use ($app){
+                
+            })->bind('subscribe');
             
             return $controller;
-        }
-        
-        function subscribe(Application $app){
-            
         }
     }
 }

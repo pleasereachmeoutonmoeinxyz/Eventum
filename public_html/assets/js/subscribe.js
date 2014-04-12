3cc252fe0150970ea23ac118f27624532d463735
moein7tl <moein7tl@gmail.com>
2014-04-12 14:17:55 +0430

    change subscribe

--- public_html/assets/js/subscribe.js
+++ public_html/assets/js/subscribe.js
@@ -1,8 +1,8 @@
 (function(window,document,$,undefined){
     $(document).ready(function(){
         $("#subscribe button").on('click',function(){
-            $.post('subscribe',$("#subscribe form").serialize()
-            ).done(function(data){
+            $.post('/',$("#subscribe form").serialize())
+            .done(function(data){
                 console.log(data);
             }).fail(function(){
 

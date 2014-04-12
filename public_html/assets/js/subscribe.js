19e04f839d05b4e23cea8fa412acba8a92f68ff7
moein7tl <moein7tl@gmail.com>
2014-04-12 15:13:45 +0430

    change address

--- public_html/assets/js/subscribe.js
+++ public_html/assets/js/subscribe.js
@@ -1,7 +1,7 @@
 (function(window,document,$,undefined){
     $(document).ready(function(){
         $("#subscribe button").on('click',function(){
-            $.post('/',$("#subscribe form").serialize())
+            $.post('/mail/subscribe',$("#subscribe form").serialize())
             .done(function(data){
                 console.log(data);
             }).fail(function(){

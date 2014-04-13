a6f4f5332679ef12de10db66620cbf8b4c2bae77
moein7tl <moein7tl@gmail.com>
2014-04-14 01:29:59 +0430

    bootstrap mongo

--- protected/bootstrap.php
+++ protected/bootstrap.php
@@ -1,27 +1,22 @@
 <?php
 
-define( 'PATH_ROOT',            dirname( __DIR__ ) );
-define( 'PATH_PROTECTED',       PATH_ROOT       . '/protected' );
-define( 'PATH_RUNTIME',         PATH_ROOT       . '/runtime' );
-
-define( 'PATH_CACHE',           PATH_RUNTIME    . '/cache' );
-define( 'PATH_LOG',             PATH_RUNTIME    . '/log' );
-
-define( 'PATH_PUBLIC',          PATH_ROOT       . '/public_html' );
-define( 'PATH_VENDOR',          PATH_ROOT       . '/vendor' );
-
-define( 'PATH_LOCALES',         PATH_PROTECTED    . '/locals' );
-define( 'PATH_VIEWS',           PATH_PROTECTED    . '/views' );
-
 use Doctrine\MongoDB\Connection;
 use Doctrine\ODM\MongoDB\Configuration;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
 
-if ( ! file_exists($file = PATH_ROOT . '/vendor/autoload.php')) {
-    throw new RuntimeException('Install dependencies to run this script.');
-}
+$loader->add('Models',PATH_PROTECTED . '/model');
 
-$loader = require_once $file;
+$connection = new Connection($app['db']['server'],$app['db']['options']);
 
+$config = new Configuration();
+$config->setProxyDir(PATH_PROTECTED . '/model/Proxies');
+$config->setProxyNamespace('Proxies');
+$config->setHydratorDir(PATH_PROTECTED . '/model/Hydrators');
+$config->setHydratorNamespace('Hydrators');
+$config->setDefaultDB($app['db']['collection']);
+$config->setMetadataDriverImpl(AnnotationDriver::create(PATH_PROTECTED . '/model'));
 
+AnnotationDriver::registerAnnotationClasses();
+
+$dm = DocumentManager::create($connection, $config);

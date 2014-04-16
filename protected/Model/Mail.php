21fbcb291bc4b64aee867d068e4b9f305edbfa8c
moein7tl <moein7tl@gmail.com>
2014-04-16 12:37:17 +0430

    fix prePersist

--- protected/Model/Mail.php
+++ protected/Model/Mail.php
@@ -2,25 +2,53 @@
 namespace Model{
     
     use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
-    
-    /** @ODM\Document */
+    use Symfony\Component\Validator\Constraints as Assert;
+    /** 
+     * @property Integer $id
+     * @property String $email
+     * @property String $locations
+     * @property String $types
+     * @property String $categories
+     * @ODM\Document*/
     class Mail{
         
         /** @ODM\Id */
         private $id;
 
-        /** @ODM\String */
+        /** @ODM\String @ODM\Index(unique=true, order="asc") */
         private $email;
+        
+        /** @ODM\Collection @ODM\Index */
+        private $locations  =   array();
+        
+        /** @ODM\Collection @ODM\Index */
+        private $types      =   array();
 
-        public function setEmail($email){
-            $this->email    =   $email;
+        /** @ODM\Collection @ODM\Index */
+        private $categories =   array();
+
+        public function __set($name, $value) {
+            if (property_exists(__CLASS__, $name)){
+                $this->{$name}  =   $value;
+            }
         }
 
+        public function __get($name) {
+            if (property_exists(__CLASS__, $name)){
+                return $this->{$name};
+            }
+        }
+
+        /**
+         * Validate attribute by Silex validator
+         * @param \Model\ClassMetadata $metadata
+         */
         static public function loadValidatorMetaData(ClassMetadata $metadata){
             $metadata->addPropertyConstraint('email', new Assert\Email());
             $metadata->addPropertyConstraint('email', new Assert\NotBlank());
             $metadata->addPropertyConstraint('email', new Assert\Length(5,255));
         }        
         
+    
     }
 }

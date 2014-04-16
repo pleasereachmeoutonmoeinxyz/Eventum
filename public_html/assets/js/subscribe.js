061774e5bb2377d34d0e26313846a5ffb59ddd39
moein7tl <moein7tl@gmail.com>
2014-04-16 17:21:05 +0430

    change selector

--- public_html/assets/js/subscribe.js
+++ public_html/assets/js/subscribe.js
@@ -1,7 +1,7 @@
 (function(window,document,$,undefined){
     $(document).ready(function(){
         $("#subscribe button").on('click',function(){
-            $.post('/mail/subscribe',$("#subscribe form").serialize())
+            $.post('mail/subscribe',$("form#subscribe").serialize())
             .done(function(data){
                 console.log(data);
             }).fail(function(){

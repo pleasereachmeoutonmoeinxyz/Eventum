ad9800da9263586c8da2b023ce91095f493201fd
moein7tl <moein7tl@gmail.com>
2014-04-16 16:59:28 +0430

    add error code

--- protected/EventMail.php
+++ protected/EventMail.php
@@ -3,7 +3,7 @@
 class EventMail {
     
     private static $instance    =   null;
-    
+    const VALIDATOR_ERROR       =   500;
     public static function app(){
         if (!self::$instance instanceof \Silex\Application)
             self::$instance    =   new \Silex\Application;

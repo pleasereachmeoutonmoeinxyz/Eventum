bcc377b8078cd409768958fa4807cddaa4471c8d
moein7tl <moein7tl@gmail.com>
2014-04-18 16:18:41 +0430

    dummy

--- protected/Model/Base.php
+++ protected/Model/Base.php
@@ -1,31 +1,29 @@
 <?php
 namespace Model {
-    
+        
     class Base{
         
         public static function findOne($params = array()) {
             $app    = \EventMail::app();
-            return $app['dm']->getRepository(get_called_class())
-                    ->findOneBy($params);
+            return $app['dm']->getRepository(get_called_class())->findOneBy($params);
         }
     
         public static function find($params){
             $app    = \EventMail::app();
-            return $app['dm']->getRepository(get_called_class())
-                    ->findBy($params);
+            return $app['dm']->getRepository(get_called_class())->findBy($params);
         }
         
         public function getErrors(){
             $app    =   \EventMail::app();
-            $errors =   $app['validator']->validate($this);
-            if (count($errors) === 0)
+            $errors = $app['validator']->validate($this);
+            if (count($errors) == 0)
                 return NULL;
             $messages   =   array();
             foreach ($errors as $error){
                 $messages[] =   $error->getMessage();
             }
             return $messages;
-        }
+        }           
     }
     
 }

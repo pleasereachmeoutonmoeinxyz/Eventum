6d99e2a5122b577aff16e3e363ac030376b99c56
moein7tl <moein7tl@gmail.com>
2014-04-21 23:49:02 +0430

    add translation message

--- protected/controller/MailController.php
+++ protected/controller/MailController.php
@@ -14,7 +14,7 @@ namespace Controller{
                     $app->abort(400);
                 
                 $mail    = \Model\Mail::getLinkParams($data['email']);
-                return $this->subscribeResonse($mail);
+                return $this->subscribeResonse($app,$mail);
 
             })->bind('subscribe');
             return $controller;
@@ -22,30 +22,32 @@ namespace Controller{
         
         /**
          * generate subscribtion json response
+         * @param Application $app
          * @param \Model\Mail $mail
          * @return string
          */
-        private function subscribeResonse($mail){
+        private function subscribeResonse(Application $app,$mail){
             if ($mail instanceof \Model\Mail){
                 if ((time() - $mail->cTimestamp) < $app['activation.time_limit']){
-                    return \Helper\Ajax::error($code, $message, $data);
+                    return \Helper\Ajax::error(NULL, $app['translator']->trans('ctrl.mail.subscribe.time_limit'), NULL);
                 } else {
-                    return $this->sendSettingMail($mail);
+                    return $this->sendSettingMail($app,$mail);
                 }
             } elseif ($mail instanceof \Model\Errors) {
-                return \Helper\Ajax::error($code, $message, $data);
+                return \Helper\Ajax::error(NULL, $app['translator']->trans('ctrl.mail.subscribe.errors'), $mail->getErrors());
             }            
         }
         /**
          * send setting url to user mail address and return json for ajax response
+         * @param Application $app
          * @param \Model\Mail $mail
          * @return string
          */
-        private function sendSettingMail($mail){
-            if (Helper\Mailer::settingMailUrl($mail->email, $mail->id, $mail->link)){
-                return \Helper\Ajax::message($code, $message, $data);
+        private function sendSettingMail(Application $app,$mail){
+            if (\Helper\Mailer::settingMailUrl($mail->email, $mail->id, $mail->link)){
+                return \Helper\Ajax::message(NULL, $app['translator']->trans('ctrl.mail.subscribe.sent_successfully'), NULL);
             } else {
-                return \Helper\Ajax::error($code, $message, $data);
+                return \Helper\Ajax::error(NULL, $app['translator']->trans('ctrl.mail.subscribe.sent_error'), NULL);
             }            
         }
     }

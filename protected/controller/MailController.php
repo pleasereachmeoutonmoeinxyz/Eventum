6b738a80c989d03a512cbf66f661a0b1f3b13b0b
moein7tl <moein7tl@gmail.com>
2014-04-21 22:35:17 +0430

    define subscribeResponse and sendSettingMail method

--- protected/controller/MailController.php
+++ protected/controller/MailController.php
@@ -10,15 +10,43 @@ namespace Controller{
             $controller =   $app['controllers_factory'];
             
             $controller->post('/subscribe',  function (Request $request) use ($app){
-                if (!$data   =   $request->get('form')){
+                if (!$data   =   $request->get('form'))
                     $app->abort(400);
-                }
                 
-                $params    = \Model\Mail::getLinkParams($data['email']);
-                
-                return "X";
+                $mail    = \Model\Mail::getLinkParams($data['email']);
+                return $this->subscribeResonse($mail);
+
             })->bind('subscribe');
             return $controller;
+        }
+        
+        /**
+         * generate subscribtion json response
+         * @param \Model\Mail $mail
+         * @return string
+         */
+        private function subscribeResonse($mail){
+            if ($mail instanceof \Model\Mail){
+                if ((time() - $mail->cTimestamp) < $app['activation.time_limit']){
+                    return \Helper\Ajax::error($code, $message, $data);
+                } else {
+                    return $this->sendSettingMail($mail);
+                }
+            } elseif ($mail instanceof \Model\Errors) {
+                return \Helper\Ajax::error($code, $message, $data);
+            }            
+        }
+        /**
+         * send setting url to user mail address and return json for ajax response
+         * @param \Model\Mail $mail
+         * @return string
+         */
+        private function sendSettingMail($mail){
+            if (Helper\Mailer::settingMailUrl($mail->email, $mail->id, $mail->link)){
+                return \Helper\Ajax::message($code, $message, $data);
+            } else {
+                return \Helper\Ajax::error($code, $message, $data);
+            }            
         }
     }
 }

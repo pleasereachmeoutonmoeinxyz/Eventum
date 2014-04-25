40334c0080f1cd2eeb04caea63c567fd7f9e6e05
moein7tl <moein7tl@gmail.com>
2014-04-25 13:48:26 +0430

    add static translation and url generator methods

--- protected/EventMail.php
+++ protected/EventMail.php
@@ -5,9 +5,33 @@ class EventMail {
     private static $instance    =   null;
     const VALIDATOR_ERROR       =   500;
     const LIMITATION_ERROR      =   501;
+    
+    /**
+     * return running instance of Application
+     * @return \Silex\Application
+     */
     public static function app(){
         if (!self::$instance instanceof \Silex\Application)
             self::$instance    =   new \Silex\Application;
         return self::$instance;
+    }
+    
+    /**
+     * translate text using TranslationServiceProvider
+     * @param string $text
+     * @return string
+     */
+    public static function t($text){
+        return self::app()->trans($text);
+    }
+    
+    /**
+     * return url using UrlGeneratorServiceProvider
+     * @param string $router
+     * @param mixed $params
+     * @return string
+     */
+    public static function url($router,$params){
+        return self::app()->url($router,$params);
     }
 }

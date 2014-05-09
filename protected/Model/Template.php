16ceec10a42a3db967e95e4d9998877550943048
moein7tl <moein7tl@gmail.com>
2014-05-09 14:57:01 +0430

    init Event and Template model

---
+++ protected/Model/Template.php
@@ -0,0 +1,12 @@
+<?php
+namespace Model{
+    
+    use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+    use Symfony\Component\Validator\Constraints as Assert;
+    use Symfony\Component\Validator\Mapping\ClassMetadata;    
+    
+    /* @ODM\Document*/
+    class Template extends Base{
+        
+    }
+}

a10b8266fcee4f22cd5c2d41f5666d51027616ee
moein7tl <moein7tl@gmail.com>
2014-05-17 16:20:01 +0430

    complete config

--- cronjob/config.php
+++ cronjob/config.php
@@ -1,8 +1,10 @@
 <?php
 return array(
-    'SERVER'    =>  'localhost',
-    'PORT'      =>  5672,
-    'USERNAME'  =>  'guest',
-    'PASSWORD'  =>  'guest',
-    'CHANNEL'   =>  'MAIL_CHANNEL'
+    'SERVER'        =>  'localhost',
+    'PORT'          =>  5672,
+    'USERNAME'      =>  'guest',
+    'PASSWORD'      =>  'guest',
+    'CHANNEL'       =>  'MAIL_CHANNEL',
+    'DB_STRING'     =>  'mongodb://moein:12345@localhost:27017',
+    'DB_COLLECTION' =>  'eventmail'
 );

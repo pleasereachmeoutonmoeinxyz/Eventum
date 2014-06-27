00d0c0b15511fb1304b54ddac505c848dadc4ac8
moein7tl <moein7tl@gmail.com>
2014-06-27 12:03:16 +0430

    set time limit to 0

--- cronjob/worker_cronjob.php
+++ cronjob/worker_cronjob.php
@@ -1,4 +1,5 @@
 <?php
+set_time_limit(0);
 include_once (dirname(__FILE__))."/cron.helper.php";
 include_once (dirname( __DIR__ )."/vendor/autoload.php");
 $config = include_once (dirname(__FILE__))."/config.php";
